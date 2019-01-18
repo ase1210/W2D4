4d1f1d1da0a91448f4c826b872cf638c692d27cf
DrewE <andrew.engelstein@gmail.com>
2019-01-17 16:40:30 -0800

    Add #okay_two_sum? with binary search

---
+++ Two Sum/two_sum.rb
@@ -0,0 +1,50 @@
+# Two Sum
+
+# Brute Force
+# time complextiy: O(n^2)
+# space complextiy: O(n^2)
+
+def bad_two_sum?(arr, target)
+  sums = []
+  arr.each_index do |i|
+    (i+1...arr.length).each do |j|
+      sums << arr[i] + arr[j]
+    end
+  end
+  sums.include?(target)
+end
+
+# Sorting
+
+def okay_two_sum?(arr, target)
+  sorted = arr.sort 
+  arr.each_with_index do |el, idx|
+    new_arr = arr[0...idx]+arr[idx+1..-1]
+    result = my_bs(new_arr, target-el)
+    return true if result
+  end
+  false
+end
+
+def my_bs(array, target)
+  return nil if array.length == 1 && array[0] != target
+  mid = array.length/2
+  
+  if array[mid] == target
+    return mid 
+  elsif target < array[mid]
+    return my_bs(array[0...mid], target)
+  else
+    result = my_bs(array[mid+ 1..-1], target)
+    if result 
+      return mid + 1 + result 
+    else 
+      return nil
+    end
+  end
+end
+
+arr = [0, 1, 5, 7]
+p okay_two_sum?(arr, 6) # => should be true
+p okay_two_sum?(arr, 10) # => should be false
+
